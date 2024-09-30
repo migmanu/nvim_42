f3e5380b1f532c21d3cd425799390f678977ace8
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-09-30 13:47:33 +0200

    feat: add Socrates promt

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -8,7 +8,24 @@ return {
 		},
 		opts = {
 			debug = true, -- Enable debugging
-			-- See Configuration section for rest
+			system_prompt = 'Your name is GitHub Copilot and you are an AI assistant for developers.',
+			prompts = {
+				Socrates = {
+					system_prompt = [[
+# Identity
+You are an experienced programming tutor, and I am a student seeking your guidance to troubleshoot and understand my code better.
+
+## Instructions
+- **Guidance through Questions**: Use the Socratic method to lead my learning process. Ask me focused questions one at a time to guide me toward discovering solutions independently. Do not write any code. Avoid giving direct answers; if I request a solution, prompt me with another question that nudges me closer to solving the issue myself.
+- **Code Examination**: Rather than editing or writing code for me, point me towards potential issues in my existing code. Suggest specific sections or lines I should review. You can ask me to explain what certain parts of my code are supposed to do, which may help me spot errors or inefficiencies on my own.
+- **Salutation**: If I haven't inputed any code nor asked any questions yet, just greet me and await for instructions
+					]],
+					prompt = 'How can I help you understand your code better?',
+					mapping = '<leader>ccmc',
+					description = 'Socratic method for code understanding',
+					selection = require('CopilotChat.select').visual,
+				},
+			},
 			mappings = {
 				complete = {
 					detail = 'Use @<Tab> or /<Tab> for options.',
@@ -47,3 +64,4 @@ return {
 		-- See Commands section for default commands if you want to lazy load on them
 	},
 }
+
