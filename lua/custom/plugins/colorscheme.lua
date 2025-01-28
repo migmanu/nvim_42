6c91983ac45300c698dc2cc36f7ef44351618eb3
migmanu <jmanuelmigoya@gmail.com>
2025-01-28 20:21:51 +0100

    feat(colorscheme): switch to onedark theme
    Replaced modus-themes.nvim with onedark.nvim in the colorscheme
    configuration. This change updates the theme to onedark, which is
    inspired by Atom. The priority and configuration settings have been
    adjusted accordingly.

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -7,14 +7,14 @@ return {
 	{ "navarasu/onedark.nvim" },
 	{ "miikanissi/modus-themes.nvim" },
 
-  {
-    -- Theme inspired by Atom
-    "miikanissi/modus-themes.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.opt.background = "light" -- set this to dark or light
-      vim.cmd.colorscheme("modus_operandi")
-      -- vim.cmd("highlight MatchParen guifg=pink guibg=grey")
-    end,
-  },
+	{
+		-- Theme inspired by Atom
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			-- vim.opt.background = "light" -- set this to dark or light
+			vim.cmd.colorscheme("onedark")
+			-- vim.cmd("highlight MatchParen guifg=pink guibg=grey")
+		end,
+	},
 }
